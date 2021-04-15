50352f49f6ad1e9127458455512e6f70611e10cd
Silviu <silviu.manea.hamzau@outlook.com>
2021-04-15 17:28:11 +0200

    2-23-Feature-Getting-a-Task-by-ID

--- nestjs-task-management/src/tasks/tasks.service.ts
+++ nestjs-task-management/src/tasks/tasks.service.ts
@@ -5,12 +5,20 @@ import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
+  //Tasks array - fake BBDD
   private tasks: Task[] = [];
 
+  //Returns all tasks
   getAllTasks(): Task[] {
     return this.tasks;
   }
 
+  //Returns only the task with a specific id, if the id matches
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  //Create a new task and assigns a unique id
   createTask(createTaskDto: CreateTaskDto): Task {
     //Extract only the keys we need from the dto
     const { title, description } = createTaskDto;
