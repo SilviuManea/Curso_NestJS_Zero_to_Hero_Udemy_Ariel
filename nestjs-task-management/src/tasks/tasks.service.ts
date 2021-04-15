14fa664a0715ee74db1185c0b1c32ab514cbc17a
Silviu <silviu.manea.hamzau@outlook.com>
2021-04-15 17:01:17 +0200

    2-22-Creating a CreateTaskDto

--- nestjs-task-management/src/tasks/tasks.service.ts
+++ nestjs-task-management/src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, ParseUUIDPipe } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,7 +11,10 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  createTask(createTaskDto: CreateTaskDto): Task {
+    //Extract only the keys we need from the dto
+    const { title, description } = createTaskDto;
+
     const task: Task = {
       id: uuid(),
       title,
