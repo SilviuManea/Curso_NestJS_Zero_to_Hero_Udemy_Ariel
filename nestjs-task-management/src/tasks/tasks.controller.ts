fb38ca162be11dbd94a090a8d1f086ee8578350e
Silviu <silviu.manea.hamzau@outlook.com>
2021-04-21 12:55:48 +0200

    4-44-Preparation for Task Service Refactoring

--- nestjs-task-management/src/tasks/tasks.controller.ts
+++ nestjs-task-management/src/tasks/tasks.controller.ts
@@ -14,51 +14,50 @@ import {
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation-pipe';
-import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
-  @Get()
-  getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
-    if (Object.keys(filterDto).length) {
-      //if we have any argument do this
-      return this.tasksService.getTasksWithFilters(filterDto);
-    } else {
-      //if no filtering arguments provided to this
-      //console.log(filterDto); //->try it atacking http://localhost:3000/tasks?status=OPEN&search=hello
-      return this.tasksService.getAllTasks();
-    }
-  }
-
-  @Get('/:id')
-  getTaskById(@Param('id') id: string): Task {
-    return this.tasksService.getTaskById(id);
-  }
-
-  @Post()
-  @UsePipes(ValidationPipe)
-  createTask(@Body() createTaskDto: CreateTaskDto): Task {
-    return this.tasksService.createTask(createTaskDto);
-  }
-
-  @Delete('/:id')
-  deleteTask(@Param('id') id: string): void {
-    this.tasksService.deleteTask(id);
-  }
-  // @Delete('/:id')//my Solution
-  // deleteTaskById(@Param('id') id: string): void {
-  //   this.tasksService.deleteTaskById(id);
+  // @Get()
+  // getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
+  //   if (Object.keys(filterDto).length) {
+  //     //if we have any argument do this
+  //     return this.tasksService.getTasksWithFilters(filterDto);
+  //   } else {
+  //     //if no filtering arguments provided to this
+  //     //console.log(filterDto); //->try it atacking http://localhost:3000/tasks?status=OPEN&search=hello
+  //     return this.tasksService.getAllTasks();
+  //   }
   // }
 
-  //Patch - Tutorial Solution
-  @Patch('/:id/status')
-  updateTaskStatus(
-    @Param('id') id: string,
-    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
-  ): Task {
-    return this.tasksService.updateTaskStatus(id, status);
-  }
+  // @Get('/:id')
+  // getTaskById(@Param('id') id: string): Task {
+  //   return this.tasksService.getTaskById(id);
+  // }
+
+  // @Post()
+  // @UsePipes(ValidationPipe)
+  // createTask(@Body() createTaskDto: CreateTaskDto): Task {
+  //   return this.tasksService.createTask(createTaskDto);
+  // }
+
+  // @Delete('/:id')
+  // deleteTask(@Param('id') id: string): void {
+  //   this.tasksService.deleteTask(id);
+  // }
+  // // @Delete('/:id')//my Solution
+  // // deleteTaskById(@Param('id') id: string): void {
+  // //   this.tasksService.deleteTaskById(id);
+  // // }
+
+  // //Patch - Tutorial Solution
+  // @Patch('/:id/status')
+  // updateTaskStatus(
+  //   @Param('id') id: string,
+  //   @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+  // ): Task {
+  //   return this.tasksService.updateTaskStatus(id, status);
+  // }
 }
--- nestjs-task-management/src/tasks/tasks.service.ts
+++ nestjs-task-management/src/tasks/tasks.service.ts
@@ -1,94 +1,76 @@
 import { Injectable, NotFoundException, ParseUUIDPipe } from '@nestjs/common';
-import { Task, TaskStatus } from './task.model';
+
 import { v1 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
-  //Tasks array - fake BBDD
-  private tasks: Task[] = [];
-
-  //Returns all tasks
-  getAllTasks(): Task[] {
-    return this.tasks;
-  }
-
-  //Returns only the task with a specific id, if the id matches
-  getTaskById(id: string): Task {
-    const found = this.tasks.find((task) => task.id === id);
-
-    if (!found) {
-      throw new NotFoundException(`Task with ID "${id}" not found`);
-    }
-
-    return found;
-  }
-
-  //Create a new task and assigns a unique id
-  createTask(createTaskDto: CreateTaskDto): Task {
-    //Extract only the keys we need from the dto
-    const { title, description } = createTaskDto;
-
-    const task: Task = {
-      id: uuid(),
-      title,
-      description,
-      status: TaskStatus.OPEN,
-    };
-
-    this.tasks.push(task);
-    return task;
-  }
-
-  // Delete a task by its ID - My solution
-  // deleteTaskById(id: string) {
-  //   let indice = 0;
-  //   const id_to_delete = this.tasks.find((element, index) => {
-  //     indice = index;
-  //     return element.id === id;
-  //   });
-
-  //   console.log(id_to_delete);
-  //   console.log(indice);
-
-  //   this.tasks.splice(indice, 1);
-
-  //   return 'Id of deleted element was' + id_to_delete.id;
+  // //Returns all tasks
+  // getAllTasks(): Task[] {
+  //   return this.tasks;
   // }
-
-  //Delete a task by its ID - Tutorial's solution
-  deleteTask(id: string): void {
-    const found = this.getTaskById(id);
-    this.tasks = this.tasks.filter((task) => task.id !== found.id); //if the function returns false, that task is filtered out of the array(we return it only for the tasks with the id we want to delete)
-  }
-  //Update Tasks Status
-  updateTaskStatus(id: string, status: TaskStatus) {
-    const task = this.getTaskById(id);
-    task.status = status;
-    return task;
-  }
-
-  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-    //atack endpoint passing some filters http://localhost:3000/tasks?status=OPEN&search=NestJS
-    const { status, search } = filterDto; //dto destruct -> equals const status = filterDto.status; , same with search.
-
-    //get all tasks
-    let tasks = this.getAllTasks();
-
-    if (status) {
-      //filter by status
-      tasks = tasks.filter((task) => task.status === status); //if it has the same status it will be included in to the array
-    }
-    if (search) {
-      //filter by search
-      tasks = tasks.filter(
-        //adds to the result only those objects that match the conditions
-        (task) =>
-          task.title.includes(search) || task.description.includes(search),
-      );
-    }
-
-    return tasks; //return array of matching objects.
-  }
+  // //Returns only the task with a specific id, if the id matches
+  // getTaskById(id: string): Task {
+  //   const found = this.tasks.find((task) => task.id === id);
+  //   if (!found) {
+  //     throw new NotFoundException(`Task with ID "${id}" not found`);
+  //   }
+  //   return found;
+  // }
+  // //Create a new task and assigns a unique id
+  // createTask(createTaskDto: CreateTaskDto): Task {
+  //   //Extract only the keys we need from the dto
+  //   const { title, description } = createTaskDto;
+  //   const task: Task = {
+  //     id: uuid(),
+  //     title,
+  //     description,
+  //     status: TaskStatus.OPEN,
+  //   };
+  //   this.tasks.push(task);
+  //   return task;
+  // }
+  // // Delete a task by its ID - My solution
+  // // deleteTaskById(id: string) {
+  // //   let indice = 0;
+  // //   const id_to_delete = this.tasks.find((element, index) => {
+  // //     indice = index;
+  // //     return element.id === id;
+  // //   });
+  // //   console.log(id_to_delete);
+  // //   console.log(indice);
+  // //   this.tasks.splice(indice, 1);
+  // //   return 'Id of deleted element was' + id_to_delete.id;
+  // // }
+  // //Delete a task by its ID - Tutorial's solution
+  // deleteTask(id: string): void {
+  //   const found = this.getTaskById(id);
+  //   this.tasks = this.tasks.filter((task) => task.id !== found.id); //if the function returns false, that task is filtered out of the array(we return it only for the tasks with the id we want to delete)
+  // }
+  // //Update Tasks Status
+  // updateTaskStatus(id: string, status: TaskStatus) {
+  //   const task = this.getTaskById(id);
+  //   task.status = status;
+  //   return task;
+  // }
+  // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+  //   //atack endpoint passing some filters http://localhost:3000/tasks?status=OPEN&search=NestJS
+  //   const { status, search } = filterDto; //dto destruct -> equals const status = filterDto.status; , same with search.
+  //   //get all tasks
+  //   let tasks = this.getAllTasks();
+  //   if (status) {
+  //     //filter by status
+  //     tasks = tasks.filter((task) => task.status === status); //if it has the same status it will be included in to the array
+  //   }
+  //   if (search) {
+  //     //filter by search
+  //     tasks = tasks.filter(
+  //       //adds to the result only those objects that match the conditions
+  //       (task) =>
+  //         task.title.includes(search) || task.description.includes(search),
+  //     );
+  //   }
+  //   return tasks; //return array of matching objects.
+  // }
 }
