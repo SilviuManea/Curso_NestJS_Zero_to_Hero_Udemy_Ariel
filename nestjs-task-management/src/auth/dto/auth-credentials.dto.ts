2ede56db7b6f137f95a711762baed28b1c85ce65
Silviu <silviu.manea.hamzau@outlook.com>
2021-04-27 17:05:02 +0200

    5-55-Validation-AuthCredentialsDto,password-strength

--- nestjs-task-management/src/auth/dto/auth-credentials.dto.ts
+++ nestjs-task-management/src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  //validation rules that are being matched on the auth controller with the ValidationPipe
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
   password: string;
 }
