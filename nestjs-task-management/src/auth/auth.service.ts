42f753cc7e66fc7e075db8f4ce2a4fdde24fbfba
Silviu <silviu.manea.hamzau@outlook.com>
2021-04-30 12:38:17 +0200

    5-58-Feature validating Password Sign In

--- nestjs-task-management/src/auth/auth.service.ts
+++ nestjs-task-management/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './user.repository';
@@ -13,5 +13,14 @@ export class AuthService {
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.signUp(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto) {
+    const username = await this.userRepository.validateUserPassword(
+      authCredentialsDto,
+    );
+    if (!username) {
+      throw new UnauthorizedException('Invalid Credentials'); //we throw this so that the atacker does not know if the user or the pwd is correcto or incorrect. We return the same message without any hint.
+    }
   }
 }
--- nestjs-task-management/src/auth/user.entity.ts
+++ nestjs-task-management/src/auth/user.entity.ts
@@ -5,6 +5,7 @@ import {
   PrimaryGeneratedColumn,
   Unique,
 } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Entity()
 @Unique(['username']) //Second Method - assuring unique columns in the database. If another user is created with the same name an 500 error will be thrown
@@ -20,4 +21,9 @@ export class User extends BaseEntity {
 
   @Column()
   salt: string;
+
+  async validatePassword(password: string): Promise<boolean> {
+    const hash = await bcrypt.hash(password, this.salt); //we hash the provided password and return true or false depending if the hash matches
+    return hash === this.password;
+  }
 }
