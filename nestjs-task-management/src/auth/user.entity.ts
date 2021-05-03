c38ef979a308754bd7dc8eaf5aa70818820b0df2
Silviu <silviu.manea.hamzau@outlook.com>
2021-05-03 17:10:26 +0200

    6-66-Tasks and Users - Database Relation

--- nestjs-task-management/src/auth/user.entity.ts
+++ nestjs-task-management/src/auth/user.entity.ts
@@ -1,11 +1,6 @@
-import {
-  BaseEntity,
-  Column,
-  Entity,
-  PrimaryGeneratedColumn,
-  Unique,
-} from 'typeorm';
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn, Unique } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import { Task } from 'src/tasks/task.entity';
 
 @Entity()
 @Unique(['username']) //Second Method - assuring unique columns in the database. If another user is created with the same name an 500 error will be thrown
@@ -21,6 +16,9 @@ export class User extends BaseEntity {
 
   @Column()
   salt: string;
+
+  @OneToMany((type) => Task, (task) => task.user, { eager: true })
+  tasks: Task[];
 
   async validatePassword(password: string): Promise<boolean> {
     const hash = await bcrypt.hash(password, this.salt); //we hash the provided password and return true or false depending if the hash matches
