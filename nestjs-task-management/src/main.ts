2d7f93ee2e2f96d5a97f789b9fad7cb1aae34a8b
Silviu <silviu.manea.hamzau@outlook.com>
2021-05-05 09:42:54 +0200

    9-80-Font end Working OK

--- nestjs-task-management/src/main.ts
+++ nestjs-task-management/src/main.ts
@@ -7,6 +7,11 @@ async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
+  //console.log(process.env.NODE_ENV);
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
   const serverConfig = config.get('server');
   //console.log(serverConfig);
 
