390c6a59e84a411278d52c7728f682b93e16b293
Silviu <silviu.manea.hamzau@outlook.com>
2021-05-06 11:41:41 +0200

    11-96-Testing TasksService - Part 4 - deleteTask

--- nestjs-task-management/src/tasks/tasks.service.spec.ts
+++ nestjs-task-management/src/tasks/tasks.service.spec.ts
@@ -13,6 +13,7 @@ const mockTaskRepository = () => ({
   getTasks: jest.fn(), //we monitorize the function to be tested
   findOne: jest.fn(),
   createTask: jest.fn(),
+  delete: jest.fn(),
 }); //we return the object
 
 describe('TasksService', () => {
@@ -72,5 +73,19 @@ describe('TasksService', () => {
       expect(taskRepository.createTask).toHaveBeenCalledWith(createTaskDto, mockUser);
       expect(result).toEqual('someTask');
     });
+  });
+
+  describe('deleteTask', () => {
+    it('calls taskRepository.deleteTask() to delete a task', async () => {
+      taskRepository.delete.mockResolvedValue({ affected: 1 });
+      expect(taskRepository.delete).not.toHaveBeenCalled();
+      await tasksService.deleteTask(1, mockUser);
+      expect(taskRepository.delete).toHaveBeenCalledWith({ id: 1, userId: mockUser.id });
+    });
+  });
+
+  it('throws an error as task could not be found', () => {
+    taskRepository.delete.mockResolvedValue({ affected: 0 });
+    expect(tasksService.deleteTask(1, mockUser)).rejects.toThrow(NotFoundException);
   });
 });
